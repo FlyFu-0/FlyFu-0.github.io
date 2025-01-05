459b91f5aa3868ffd3e2d8d031deebcd2cfdcfe4
Andrey Tihonov <tihonov-04@yandex.ru>
2025-01-05 16:52:29 +0300

    table layout

--- index.php
+++ index.php
@@ -1,7 +1,40 @@
 <?php
     require_once __DIR__ . '/header.php';
+
+    $db = db();
+
+    if (mysqli_connect_errno()) {
+
+        die("Database connection failed: " . mysqli_connect_error());
+    }
+
+    $result = mysqli_query($db, "SELECT username, email, text, m.create_date AS 'created' FROM messages m JOIN user u ON m.user_id = u.id ORDER BY 4 DESC;");
 ?>
-    <h1>Hello, World!</h1>
+<table>
+    <thead>
+        <tr>
+            <td>UserName</td>
+            <td>Email</td>
+            <td>Text</td>
+            <td>File</td>
+            <td>Created</td>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+
+        while ($row = mysqli_fetch_assoc($result)) : ?>
+            <tr>
+                <td><?= $row["username"] ?></td>
+                <td><?= $row["email"] ?></td>
+                <td><?= $row["text"] ?></td>
+                <td></td>
+                <td><?= $row["created"] ?></td>
+            </tr>
+        <?php endwhile ?>
+    </tbody>
+</table>
 <?php
+    mysqli_close($db);
     require_once __DIR__ . '/footer.php';
 ?>
