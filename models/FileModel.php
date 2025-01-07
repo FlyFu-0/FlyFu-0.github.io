60769c1cfb381e3e8791f49639d1d71a2847d20d
Andrey Tihonov <tihonov-04@yandex.ru>
2025-01-08 00:26:12 +0300

    refactor FileModel.php

--- models/FileModel.php
+++ models/FileModel.php
@@ -14,40 +14,61 @@ class FileModel
         $this->uploadPath = $_SERVER['DOCUMENT_ROOT'] . '/userfiles/';
     }
 
-    public function saveFile(&$error, &$savedfilePath): bool
+    public function saveFile(&$error, &$savedfilePath)
     {
-
         $file = $_FILES['file'];
         $fileName = $file['name'];
         $fileSize = $file['size'];
         $fileTmpPath = $file['tmp_name'];
         $fileExtension = pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
 
-        if (!in_array($fileExtension, array_merge($this->allowedTextExtensions, $this->allowedImageExtensions))) {
-            $error .= 'Unexpected file type! Allowed: TXT, JPG, GIF, PNG.';
-            return false;
-        } elseif ($fileSize > $this->maxFileSize) {
-            $error .= 'File size exceeds limit of 100KB!';
+        $error = '';
+
+        if ($this->isExtensionValid($fileExtension)) {
+            $error .= 'Unexpected file type! Allowed: TXT, JPG, GIF, PNG.' . PHP_EOL;
             return false;
         }
 
-        if (in_array($fileExtension, $this->allowedImageExtensions)) {
-            list($width, $height) = getimagesize($fileTmpPath);
-            if ($width > 320 || $height > 240) {
-                $error .= "Image exceeds allowed resolution. Allowed resolution: 320x240!";
-                return false;
-            }
+        if ($this->isSizeValid($fileSize)) {
+            $error .= 'File size exceeds limit of 100KB!' . PHP_EOL;
+            return false;
         }
 
-        $storageFileName = time() . " - " . $fileName;
+        if ($this->isImage($fileExtension) && $this->isImageResolutionValid($fileTmpPath)) {
+            $error .= "Image exceeds allowed resolution. Allowed resolution: 320x240!" . PHP_EOL;
+            return false;
+        }
+
+        $storageFileName = time() . "_" . $fileName;
         $storageFilePath = $this->uploadPath . $storageFileName;
         if (!move_uploaded_file($fileTmpPath, $storageFilePath)) {
-            $error .= "Error saving the file!";
+            $error .= "Error saving the file!" . PHP_EOL;
             return false;
         } else {
             $savedfilePath = $storageFileName;
         }
 
         return true;
+    }
+
+    private function isExtensionValid($fileExtension): bool
+    {
+        return !in_array($fileExtension, array_merge($this->allowedTextExtensions, $this->allowedImageExtensions));
+    }
+
+    private function isSizeValid($fileSize): bool
+    {
+        return $fileSize > $this->maxFileSize;
+    }
+
+    private function isImage($fileExtension): bool
+    {
+        return in_array($fileExtension, $this->allowedImageExtensions);
+    }
+
+    private function isImageResolutionValid($fileTmpPath): bool
+    {
+        list($width, $height) = getimagesize($fileTmpPath);
+        return $width > 320 || $height > 240;
     }
 }
